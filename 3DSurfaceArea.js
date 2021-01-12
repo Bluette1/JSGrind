5b0cfafb1d7168deec9b99363dfe88059a20b2ea
Bluette1 <marylene.sawyer@gmail.com>
2021-01-12 12:56:20 +0200

    Add `HappyLadybug`, `3DSurfaceArea`

---
+++ 3DSurfaceArea.js
@@ -0,0 +1,94 @@
+/**
+ * https://www.hackerrank.com/challenges/3d-surface-area/problem
+ *
+ * @param {number} board
+ */
+const sideArea = (H, W, board) => {
+  let totalArea = 0;
+  if (H === 1) {
+    let area = 0;
+    for (let row = 0; row < W; row += 1) {
+      area += 2 * board[0][row];
+    }
+    return area;
+  }
+
+  if (W === 1) {
+    let area = 0;
+    for (let row = 0; row < H; row += 1) {
+      area += 2 * board[row][0];
+    }
+    return area;
+  }
+
+  for (let row = 0; row < H; row += 1) {
+    let area = board[row][0];
+    for (let col = 1; col < W; col += 1) {
+      const exposedArea = board[row][col] - board[row][col - 1];
+      if (exposedArea > 0) {
+        area += exposedArea;
+      }
+    }
+
+    totalArea += area;
+
+    area = board[row][W - 1];
+    for (let col = W - 2; col >= 0; col -= 1) {
+      const exposedArea = board[row][col] - board[row][col + 1];
+      if (exposedArea > 0) {
+        area += exposedArea;
+      }
+    }
+    totalArea += area;
+  }
+
+  for (let col = 0; col < H; col += 1) {
+    let area = board[0][col];
+    for (let row = 1; row < W; row += 1) {
+      const exposedArea = board[row][col] - board[row - 1][col];
+      if (exposedArea > 0) {
+        area += exposedArea;
+      }
+    }
+    totalArea += area;
+
+    area = board[W - 1][col];
+
+    for (let row = W - 2; row >= 0; row -= 1) {
+      const exposedArea = board[row][col] - board[row + 1][col];
+      if (exposedArea > 0) {
+        area += exposedArea;
+      }
+    }
+    totalArea += area;
+  }
+  return totalArea;
+};
+
+const surfaceArea = (board) => {
+  const H = board.length;
+  const W = board[0].length;
+  let totalArea = 0;
+  totalArea += sideArea(H, W, board);
+  totalArea += 2 * (H * W);
+  return totalArea;
+};
+
+console.log(surfaceArea([[1]]));
+console.log(surfaceArea([
+  [1, 3, 4], [2, 2, 3], [1, 2, 4],
+]));
+
+console.log(surfaceArea([
+  [51],
+  [32],
+  [28],
+  [49],
+  [28],
+  [21],
+  [98],
+  [56],
+  [99],
+  [77],
+]));
+// 1482
--- HappyLadybugs.js
+++ HappyLadybugs.js
@@ -3,23 +3,6 @@
  *
  * @param {number} board - board string
  */
-
-const fillSpace = (board, index) => {
-  for (let idx = 0; idx < board.length; idx += 1) {
-    // if there's a space
-    if (board[idx] === '_') {
-      // check adjacent positions
-      if (board[idx - 1] === board[index] || board[idx + 1] === board[index]) {
-        // move ladybug to empty space
-        board[idx] = board[index];
-        board[index] = '_';
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
 const checkUnhappyLadybug = (board) => {
   if (board[0] !== '_' && board[0] !== board[1]) {
     return false;
@@ -35,44 +18,45 @@ const checkUnhappyLadybug = (board) => {
   }
   return true;
 };
-const unhappyLadybug = (board) => {
-  let filledSpaces = 0;
-  if (board[0] !== board[1] && board[1] !== '_') {
-    if (fillSpace(board, 0)) {
-      filledSpaces += 1;
-    }
-  }
 
-  if (board[board.length - 1] !== board[board.length - 2] && board[board.length - 2] !== '_') {
-    if (fillSpace(board, board.length - 1)) {
-      filledSpaces += 1;
-    }
+const space = (boardMap) => {
+  if (boardMap._ === undefined) {
+    return false;
   }
-  for (let idx = 1; idx < board.length - 1; idx += 1) {
-    if (board[idx] !== board[idx - 1] && board[idx] !== board[idx + 1]) {
-      // unhappy ladybug
-      if (fillSpace(board, idx)) {
-        filledSpaces += 1;
-      }
-    }
-  }
-  return filledSpaces;
+  return true;
 };
-// Complete the happyLadybugs function below.
+
+const boardMap = (board) => {
+  const map = {};
+  for (let idx = 0; idx < board.length; idx += 1) {
+    if (map[board[idx]] === undefined) {
+      map[board[idx]] = 1;
+    } else {
+      map[board[idx]] += 1;
+    }
+  }
+  return map;
+};
+
+const isMultiples = (boardMap) => {
+  const keys = Object.keys(boardMap);
+  for (let idx = 0; idx < keys.length; idx += 1) {
+    if (keys[idx] !== '_' && boardMap[keys[idx]] < 2) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const happyLadybugs = (board) => {
   board = board.split('');
-
-  while (unhappyLadybug(board) > 0) {
-    unhappyLadybug(board);
-  }
+  const boardDict = boardMap(board);
   if (checkUnhappyLadybug(board)) {
+    return 'YES';
+  }
+  if (space(boardDict) && isMultiples(boardDict)) {
     return 'YES';
   }
   return 'NO';
 };
-happyLadybugs(['Y', 'Y', 'R', '_', 'B', '_', 'B', 'R'].join(''));
-// console.log(happyLadybugs(['Y', 'Y', 'R', '_', 'B', '_', 'B', 'R'].join('')));
-// console.log(happyLadybugs('RBY_YBR'));
-// console.log(happyLadybugs('X_Y__X'));
-// console.log(happyLadybugs('__'));
-// console.log(happyLadybugs('B_RRBR'));
+happyLadybugs('B_RRBR');
