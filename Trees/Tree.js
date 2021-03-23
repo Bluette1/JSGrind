5e4020c55ed371980f2697001a70e391cdf04250
Bluette1 <marylene.sawyer@gmail.com>
2021-03-24 00:03:05 +0200

    Add documentation - README

--- Trees/Tree.js
+++ Trees/Tree.js
@@ -1,14 +1,38 @@
 class Node {
-	constructor(value, parentNode, children) {
-        this.parent = parentNode;
-        this.children = children;
-        this.value = value;
-    };
+	constructor(value, parentNode = null, children = []) {
+		this.parent = parentNode;
+		this.children = children;
+		this.value = value; 
+	};
 }
 
 class Tree {
-    add(value, parentNode = null, children = []) {
-        Node newNode = new Node (value);
+	add(value, root, parentNode = null, children = []) {;
+        if (root === null) {
+            return new Node (value);
+        }
 
-    }
+        Node newNode = new Node(value, parentNode, children);
+        parentNode.children.push(newNode);
+        return root;
+	}
+
+	printPreOrder() {
+		if (root === null) {
+				return;
+		}
+		console.log(root);
+		printPreOrder(root.children);
+	}
+
+	printPostOrder(root) {
+		if (root === null) {
+			return;
+		}
+		printPostOrder(root.children);
+		console.log(root);
+	}
 }
+
+Tree tr = new Tree();
+tr.add()  
