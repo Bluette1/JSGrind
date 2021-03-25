40e782d0fb3f96d963a7b65b48871a6ce623e424
Bluette1 <marylene.sawyer@gmail.com>
2021-03-25 13:18:12 +0200

    Some refactoring

--- Trees/Tree.js
+++ Trees/Tree.js
@@ -1,8 +1,11 @@
 const Node = require('./Node');
 
 class Tree {
-  constructor(root = null) {
-    this.root = root;
+  constructor(value = null) {
+    this.root = null;
+    if (value !== null) {
+      this.root = new Node(value);
+    }
   }
 
   add(value, parentNode = null, children = []) {
@@ -22,33 +25,35 @@ class Tree {
 
   printPreOrder(root) {
     if (root === null) {
-      return;
+      return '';
     }
-    console.log(root.value);
+    let str = `${root.value}`;
     root.children.forEach(child => {
-      this.printPreOrder(child);
+      str += ` ${this.printPreOrder(child)}`;
     });
+    return str.trim();
   }
 
   printPostOrder(root) {
     if (this.root === null) {
-      return;
+      return '';
     }
+    let str = '';
     root.children.forEach(child => {
-      this.printPostOrder(child);
+      str += `${this.printPostOrder(child)} `;
     });
-    console.log(root.value);
+    return  `${str}${root.value}`.trim();
   }
 }
-const tr = new Tree();
-tr.add(0);
+const tr = new Tree(0);
 tr.add(5, tr.root, [4]);
 tr.add(3, tr.root, [2, 0]);
 tr.add(6, tr.root, [1, 5]);
 tr.add(1, tr.root.children[1].children[0], [1]);
-tr.add(10, tr.root.children[1].children[1], []);
-console.log('Preorder:');
-tr.printPreOrder(tr.root);
-console.log('Postorder:');
+tr.add(10, tr.root.children[1].children[1]);
 
-tr.printPostOrder(tr.root);
+console.log('Preorder:');
+console.log(tr.printPreOrder(tr.root, ''));
+
+console.log('Postorder:');
+console.log(tr.printPostOrder(tr.root));
